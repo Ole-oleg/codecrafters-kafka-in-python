a708c1c9cea5345a3b48d1facc595efd81a85bef
Oleg Makarov <olegmakarov@Olegs-MacBook-Pro.local>
2024-09-27 19:37:20 +0300

    echo correlation ID

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 import socket  # noqa: F401
 
-CORR_ID = 7
+
+def echo_correlation_id(request):
+    corr_id = int.from_bytes(request[8:12], "big")
+
+    return get_response(corr_id)
 
 
-def get_response():
-    resonse_body = CORR_ID.to_bytes(4, "big")
+def get_response(corr_id):
+    resonse_body = corr_id.to_bytes(4, "big")
     response_len = len(resonse_body).to_bytes(4, "big")
 
     return response_len + resonse_body
@@ -16,13 +20,14 @@ def main():
     print("Logs from your program will appear here!")
 
     server = socket.create_server(("localhost", 9092), reuse_port=True)
-    conn, addr = server.accept()  # wait for client
+    conn, _ = server.accept()  # wait for client
 
     while True:
-        data = conn.recv(1024)
+        data = conn.recv(64)
         if not data:
             break
-        conn.send(get_response())
+        response = echo_correlation_id(data)
+        conn.send(response)
 
 
 if __name__ == "__main__":
