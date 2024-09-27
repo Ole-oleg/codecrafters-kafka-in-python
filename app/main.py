a00f4371cef9399d90c5da0c7250b9753e51967e
Oleg Makarov <olegmakarov@Olegs-MacBook-Pro.local>
2024-09-27 19:25:52 +0300

    Correlation ID

--- app/main.py
+++ app/main.py
@@ -1,5 +1,14 @@
 import socket  # noqa: F401
 
+CORR_ID = 7
+
+
+def get_response():
+    resonse_body = CORR_ID.to_bytes(4, "big")
+    response_len = len(resonse_body).to_bytes(4, "big")
+
+    return response_len + resonse_body
+
 
 def main():
     # You can use print statements as follows for debugging,
@@ -7,7 +16,13 @@ def main():
     print("Logs from your program will appear here!")
 
     server = socket.create_server(("localhost", 9092), reuse_port=True)
-    server.accept()  # wait for client
+    conn, addr = server.accept()  # wait for client
+
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        conn.send(get_response())
 
 
 if __name__ == "__main__":
